7863c43b221358ddcc3f1cd61e40b6fedcb71661
develop-build <anubhavj1357@gmail.com>
2023-01-16 00:51:34 +0530

    Make the UI mobile responsive

--- app/javascript/components/Answer.jsx
+++ app/javascript/components/Answer.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 const Answer = ({ answer, questionHandler }) => {
   return (
-    <div className="flex mt-4 flex-col justify-center text-justify items-center w-1/3">
-      <p className="font-bold font-serif text-lg ">
-        Answer:{'\n'}
-        <span className="font-light font-serif text-md">{answer.current}</span>
+    <div className="flex flex-col justify-center text-justify items-center p-2 w-2/3">
+      <p className="font-bold font-serif text-md">
+        Answer:{' '}
+        <span className="font-normal font-serif text-sm sm:text-md">
+          {answer.current}
+        </span>
       </p>
       <button
         onClick={questionHandler}
-        className="bg-black text-md shadow rounded mt-4 text-white py-2 px-4 font-bold"
+        className="bg-black w-full md:w-1/2 text-sm sm:text-md shadow rounded mt-4 text-white py-2 px-4 font-bold"
       >
         Ask one more question
       </button>
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -43,71 +43,76 @@ const Home = () => {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center bg-gray-200 min-h-screen min-w-screen">
-      <div className="flex justify-center">
-        <div className="w-1/3">
-          <img
-            className="shadow rounded-lg max-w-full h-auto align-middle border-none"
-            src="https://ik.imagekit.io/tirg62dow/animal_farm_gCdMOIAeJ.png?ik-sdk-version=javascript-1.4.3&updatedAt=1673687918305"
-          />
+    <div className=" bg-gray-200">
+      <div className="flex flex-col justify-center items-center min-h-screen mx-auto max-w-xl lg:max-w-3xl">
+        <div className="flex justify-center">
+          <div className="w-1/2 p-4">
+            <img
+              className="shadow rounded-lg max-w-full h-auto align-middle border-none"
+              src="https://ik.imagekit.io/tirg62dow/animal_farm_gCdMOIAeJ.png?ik-sdk-version=javascript-1.4.3&updatedAt=1673687918305"
+            />
+          </div>
         </div>
-      </div>
-      <h1 className="font-serif font-bold text-2xl my-4">Ask A Book</h1>
-      <p className="font-serif text-md font-medium mb-5 text-slate-600 text-justify leading-7 w-1/3">
-        This is an experiment in using{' '}
-        <span className="font-mono font-medium">AI</span> to make a book's
-        content more accessible. The book of choice right now is{' '}
-        <span className="font-bold">Animal Farm</span>, by George Orwell. Ask a
-        question and <span className="font-mono font-medium">AI</span>'ll answer
-        it in real-time:
-      </p>
-      <form
-        onSubmit={handleQuestionSubmit}
-        className="flex flex-col items-center w-1/3"
-      >
-        <textarea
-          ref={textArea}
-          className="p-4 text-md resize-none form-control w-full rounded-lg shadow-md disabled:shadow-lg font-normal font-mono disabled:bg-slate-300"
-          rows="3"
-          placeholder="Type in a question"
-          value={query}
-          onChange={handleTextChange}
-          type="text"
-          id="question_input"
-          name="question"
-          disabled={textAreaDisabled.current}
-        />
-        <div
-          className="mt-4 font-serif"
-          hidden={false || askQuestion.isLoading || answerVisible.current}
+        <h1 className="font-serif font-bold text-2xl">Ask A Book</h1>
+        <p className="font-serif text-sm md:text-md font-light text-slate-600 text-justify leading-5 p-2 w-2/3">
+          This is an experiment in using{' '}
+          <span className="font-mono font-medium">AI</span> to make a book's
+          content more accessible. The book of choice right now is{' '}
+          <span className="font-bold">Animal Farm</span>, by George Orwell. Ask
+          a question and <span className="font-mono font-medium">AI</span>'ll
+          answer it in real-time:
+        </p>
+        <form
+          onSubmit={handleQuestionSubmit}
+          className="flex flex-col items-center w-2/3"
         >
-          <button className="bg-black text-md shadow rounded text-white py-2 px-4 font-bold">
-            Ask question
-          </button>
-          <button className="bg-gray-100 text-md shadow rounded px-4 py-2 ml-2 font-bold">
-            I'm feeling Lucky
-          </button>
-        </div>
-      </form>
-      {answerVisible.current && !askQuestion.isLoading ? (
-        <Answer answer={answer} questionHandler={handleMoreQuestion} />
-      ) : (
-        ''
-      )}
-      {askQuestion.isLoading ? (
-        <div className="flex mt-4 gap-2 flex-col items-center justify-center">
-          <img
-            alt="Loading"
-            className="w-auto h-32 bg-transparent rounded"
-            src={loadingGifs[Math.floor(Math.random() * loadingGifs.length)]}
+          <textarea
+            ref={textArea}
+            className="p-2 text-sm md:text-md resize-y md:resize-none form-control w-full sm:h-auto h-24 rounded-lg shadow-md disabled:shadow-lg font-normal font-mono disabled:bg-slate-300"
+            rows="4"
+            placeholder="Type in a question"
+            value={query}
+            onChange={handleTextChange}
+            type="text"
+            id="question_input"
+            name="question"
+            disabled={textAreaDisabled.current}
           />
-          <p className="font-serif font-bold text-base">
-            {loadingText[Math.floor(Math.random() * loadingText.length)]}
-          </p>
-        </div>
-      ) : (
-        ''
-      )}
+          <div
+            className={`${
+              false || askQuestion.isLoading || answerVisible.current
+                ? 'hidden'
+                : ''
+            } mt-4 font-serif flex flex-col sm:flex-row gap-2 min-w-full justify-center items-center`}
+          >
+            <button className="bg-black basis-1/2 w-full sm:w-auto text-sm sm:text-md shadow rounded-lg text-white px-4 py-2 font-bold">
+              Ask question
+            </button>
+            <button className="bg-gray-100 basis-1/2 w-full sm:w-auto text-sm sm:text-md shadow rounded-lg px-4 py-2 font-bold">
+              I'm feeling Lucky
+            </button>
+          </div>
+        </form>
+        {answerVisible.current && !askQuestion.isLoading ? (
+          <Answer answer={answer} questionHandler={handleMoreQuestion} />
+        ) : (
+          ''
+        )}
+        {askQuestion.isLoading ? (
+          <div className="flex mt-4 gap-2 flex-col items-center justify-center">
+            <img
+              alt="Loading"
+              className="w-auto h-32 bg-transparent rounded"
+              src={loadingGifs[Math.floor(Math.random() * loadingGifs.length)]}
+            />
+            <p className="font-serif font-bold text-base">
+              {loadingText[Math.floor(Math.random() * loadingText.length)]}
+            </p>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   )
 }
